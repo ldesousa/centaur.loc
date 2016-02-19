2d12d01a2cb539833df3b4e707a781828e2013d0
Luís de Sousa <luis.de.sousa@protonmail.ch>
2016-02-19 12:06:02 +0100

    New tables to store gate candidates and flooded links.

--- db/0400_gate_candidates.sql
+++ db/0400_gate_candidates.sql
@@ -1,1 +1,16 @@
 ﻿
+-- Identify outlets
+SELECT *
+  FROM centaur.node
+ WHERE id
+NOT IN (SELECT id_node_from
+          FROM centaur.link);
+
+SELECT * FROM centaur.outfall;
+
+
+-- Gawasser-See
+
+SELECT *
+  FROM centaur.link
+ WHERE id_node_to = 2047439543;
