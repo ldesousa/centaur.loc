e242b8251d2eee55bcad788cc9541b824bdd7375
Luís de Sousa <luis.de.sousa@protonmail.ch>
2016-04-01 11:53:40 +0200

    Added GRANTs script.

---
+++ db/0602_grants.sql
@@ -0,0 +1,16 @@
+﻿GRANT USAGE ON SCHEMA centaur to centaur;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA centaur TO centaur;
+
+GRANT SELECT ON centaur.v_candidate TO centaur;
+GRANT SELECT ON centaur.v_conduit TO centaur;
+GRANT SELECT ON centaur.v_flooded TO centaur;
+GRANT SELECT ON centaur.v_junction TO centaur;
+GRANT SELECT ON centaur.v_outfall TO centaur;
+GRANT SELECT ON centaur.v_pump TO centaur;
+GRANT SELECT ON centaur.v_storage TO centaur;
+GRANT SELECT ON centaur.v_weir TO centaur;
+ 
+
+
+
