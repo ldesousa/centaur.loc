4610b3b4a64f1d53f2492060464bcfc529d027b8
Luís de Sousa <luis.de.sousa@protonmail.ch>
2016-08-16 16:08:44 +0200

    Subgraph routine fully functional.

--- db/0800_sub_graph.sql
+++ db/0800_sub_graph.sql
@@ -3,37 +3,35 @@
 -- Inspired on: http://stackoverflow.com/a/54362/2066215
 -- Related: http://stackoverflow.com/q/38975449/2066215
 
-
-create or replace function node_subtree (integer)
-returns setof node as
+CREATE OR REPLACE FUNCTION node_subgraph (INTEGER)
+RETURNS SETOF node AS
 $BODY$
-DECLARE results record;
-        upstream record;
+DECLARE upstream record;
 	temp RECORD;
 BEGIN
-  SELECT INTO results n.id, n.elevation, n.name, n.taken, n.geom 
+  RETURN QUERY
+  SELECT n.id, n.elevation, n.name, n.taken, n.geom 
     FROM node n,
 	 link l
    WHERE l.id_node_to = $1
      AND l.id_node_from = n.id;
   IF FOUND THEN
-    RETURN NEXT results;
     FOR upstream IN SELECT id_node_from AS id
                       FROM link
                      WHERE id_node_to = $1
       LOOP
-        FOR temp IN SELECT * FROM node_subtree(upstream.id)
+	RAISE NOTICE 'Going after node %', upstream.id;
+        FOR temp IN SELECT * FROM node_subgraph(upstream.id)
         LOOP
           RETURN NEXT temp;
         END LOOP;
       END LOOP;
   END IF;
-  --RETURN NEXT ;
+  RETURN;
 END;
 $BODY$ LANGUAGE plpgsql STABLE;
 
+
 SELECT * FROM node_subgraph(101355);
-
-
 
 
