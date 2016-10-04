11e1fa2c5cf0ba73222deea99b0dbab132ebe193
Luís de Sousa <luis.de.sousa@protonmail.ch>
2016-10-04 17:48:16 +0200

    Toulouse: import script for Subcatchments.

---
+++ db/10010_tl_subcatchments.sql
@@ -0,0 +1,52 @@
+﻿DELETE FROM toulouse.subcatchment;
+
+-- Identify duplicate names
+SELECT id, COUNT(*) AS count 
+  FROM veolia.subcatchment 
+ GROUP BY id 
+HAVING COUNT(*) > 1;
+
+-- Fix duplicates
+UPDATE veolia.subcatchment
+   SET id = id + 10000000
+ WHERE gid = (SELECT gid 
+                FROM veolia.subcatchment 
+               WHERE id = 14021 
+               LIMIT 1);
+
+UPDATE veolia.subcatchment
+   SET id = id + 10000000
+ WHERE gid = (SELECT gid 
+                FROM veolia.subcatchment 
+               WHERE id = 22008 
+               LIMIT 1);
+
+UPDATE veolia.subcatchment
+   SET id = id + 10000000
+ WHERE gid = (SELECT gid 
+                FROM veolia.subcatchment 
+               WHERE id = 27035 
+               LIMIT 1);
+
+UPDATE veolia.subcatchment
+   SET id = id + 10000000
+ WHERE gid = (SELECT gid 
+                FROM veolia.subcatchment 
+               WHERE id = 35062 
+               LIMIT 1);
+
+
+-- Make SRID is correct
+SELECT UpdateGeometrySRID('veolia.subcatchments','geom', 3035);
+
+-- Insert the data
+INSERT INTO toulouse.subcatchment (id, name, geom)
+SELECT gid, id, (ST_DUMP(geom)).geom::geometry(Polygon, 3035)
+  FROM veolia.subcatchment;
+
+
+
+
+
+
+
