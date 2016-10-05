abd8e713910b9db1fd411900973a04b17f8219eb
Luís de Sousa <luis.de.sousa@protonmail.ch>
2016-10-05 15:13:40 +0200

    Toulouse: import script for Outfalls.

---
+++ db/10020_tl_outfalls.sql
@@ -0,0 +1,22 @@
+﻿DELETE FROM toulouse.outfall;
+DELETE FROM toulouse.node WHERE id < 1000;
+
+
+-- Make sure SRID is correct
+SELECT UpdateGeometrySRID('veolia', 'subcatchment','geom', 3035);
+
+-- Insert nodes
+INSERT INTO toulouse.node (id, geom, elevation, name)
+SELECT gid + 1000000, 
+       ST_Force2d(ST_SetSRID(geom, 3035)), 
+       ST_Z(geom), 
+       gid 
+  FROM veolia.outfall;
+
+-- Insert outfalls
+INSERT INTO toulouse.outfall (id_node, type, stage_date, gated)
+SELECT gid + 1000000, 
+       typres,
+       annee_pose,
+       NULL
+  FROM veolia.outfall;
