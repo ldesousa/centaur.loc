4eb518144afadeb4d9622f61b7d65cf8e735de23
Luís de Sousa <luis.de.sousa@protonmail.ch>
2016-11-22 17:01:39 +0100

    Fixed slope calculation in v_conduit.

--- db/0500_views.sql
+++ db/0500_views.sql
@@ -40,7 +40,8 @@ SELECT l.id,
        link l	
  WHERE p.id_link = l.id;
 
-CREATE OR REPLACE VIEW v_conduit AS
+
+CREATE OR REPLACE VIEW v_conduit_common AS
 SELECT l.id,
        l.name,
        l.id_node_from,
@@ -52,53 +53,51 @@ SELECT l.id,
        c.out_offset,
        c.init_flow,
        c.max_flow,
-       slope,
+       (nf.elevation - nt.elevation) / 
+        sqrt((st_y(st_pointn(l.geom, 2)) - st_y(st_pointn(l.geom, 1)))^2 + 
+             (st_x(st_pointn(l.geom, 2)) - st_x(st_pointn(l.geom, 1)))^2) AS slope
+  FROM conduit c,
+       link l,
+       xsection x,
+       node nf,
+       node nt
+ WHERE c.id_link = l.id
+   AND x.id_link = l.id
+   AND nf.id = l.id_node_from
+   AND nt.id = l.id_node_to;
+
+-- SELECT * FROM v_conduit_common;
+
+CREATE OR REPLACE VIEW v_conduit AS
+SELECT c.*,
        area,
        perimeter,
        area * c.length AS volume,
        1.0/0.015 * power(area / perimeter, 2.0/3.0) * power(abs(slope), 1.0/2.0) AS q_max
-  FROM conduit c,
-       link l,
+  FROM v_conduit_common c,
        xsection x,
-       LATERAL (SELECT (st_y(st_pointn(l.geom, 2)) - st_y(st_pointn(l.geom, 1))) / 
-                       (st_x(st_pointn(l.geom, 2)) - st_x(st_pointn(l.geom, 1))), 
-                       pi() * ((x.geom1/2) ^ 2),
+       LATERAL (SELECT pi() * ((x.geom1/2) ^ 2),
                        2 * pi() * (x.geom1/2) )
-		    AS s1(slope, area, perimeter)
- WHERE c.id_link = l.id
-   AND x.id_link = l.id
+		    AS s1(area, perimeter)
+ WHERE x.id_link = c.id
    AND x.shape LIKE 'CIRC%'
    AND x.geom1 IS NOT NULL
  UNION
-SELECT l.id,
-       l.name,
-       l.id_node_from,
-       l.id_node_to,
-       l.geom,
-       c.length,
-       c.roughness,
-       c.in_offset,
-       c.out_offset,
-       c.init_flow,
-       c.max_flow,
-       slope,
+SELECT c.*,
        area,
        perimeter,
        area * c.length AS volume,
        1.0/0.015 * power(area / perimeter, 2.0/3.0) * power(abs(slope), 1.0/2.0) AS q_max
-  FROM conduit c,
-       link l,
+  FROM v_conduit_common c,
        xsection x,
-       LATERAL (SELECT (st_y(st_pointn(l.geom, 2)) - st_y(st_pointn(l.geom, 1))) / 
-                       (st_x(st_pointn(l.geom, 2)) - st_x(st_pointn(l.geom, 1))), 
-                       x.geom1 * x.geom2,
+       LATERAL (SELECT x.geom1 * x.geom2,
                        2 * x.geom1 + 2 * x.geom2 )
-		    AS s1(slope, area, perimeter)
- WHERE c.id_link = l.id
-   AND x.id_link = l.id
+		    AS s1(area, perimeter)
+ WHERE x.id_link = c.id
    AND x.shape LIKE 'RECT%'
-   AND x.geom1 IS NOT NULL
-   AND x.geom2 IS NOT NULL;
+   AND x.geom1 IS NOT NULL;
+
+-- SELECT * FROM v_conduit;
 
 CREATE OR REPLACE VIEW v_outfall AS
 SELECT n.id,
@@ -214,3 +213,4 @@ SELECT c.id, sum(l.volume)
 
 
 
+--SELECT * FROM coimbra.v_conduit;
