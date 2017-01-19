1c37864f27ff67e02c9087a424b791c036ce818b
Luís de Sousa <luis.de.sousa@protonmail.ch>
2017-01-19 14:36:20 +0100

    Added script to automatically fix geometry fields in ORM.

---
+++ fix_geom_fields.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git checkout HEAD src/centaur/db/Link.java
+git checkout HEAD src/centaur/db/Node.java
+git checkout HEAD src/centaur/db/Subcatchment.java
+git checkout HEAD src/centaur/db/VCandidate.java
+git checkout HEAD src/centaur/db/VConduit.java
+
+git checkout HEAD src/centaur/db/Link.hbm.xml
+git checkout HEAD src/centaur/db/Node.hbm.xml
+git checkout HEAD src/centaur/db/Subcatchment.hbm.xml
+git checkout HEAD src/centaur/db/VCandidate.hbm.xml
+git checkout HEAD src/centaur/db/VConduit.hbm.xml
