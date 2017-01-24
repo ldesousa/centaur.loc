59421424e3417e7b92b2b5070f1d0f9322123ea0
Luís de Sousa <luis.de.sousa@protonmail.ch>
2017-01-24 15:58:57 +0100

    Fixed name of VJunction class in fix_geom_fields.sh

--- fix_geom_fields.sh
+++ fix_geom_fields.sh
@@ -4,10 +4,10 @@ git checkout HEAD src/centaur/db/Link.java
 git checkout HEAD src/centaur/db/Node.java
 git checkout HEAD src/centaur/db/Subcatchment.java
 git checkout HEAD src/centaur/db/VCandidate.java
-git checkout HEAD src/centaur/db/VConduit.java
+git checkout HEAD src/centaur/db/VJunction.java
 
 git checkout HEAD src/centaur/db/Link.hbm.xml
 git checkout HEAD src/centaur/db/Node.hbm.xml
 git checkout HEAD src/centaur/db/Subcatchment.hbm.xml
 git checkout HEAD src/centaur/db/VCandidate.hbm.xml
-git checkout HEAD src/centaur/db/VConduit.hbm.xml
+git checkout HEAD src/centaur/db/VJunction.hbm.xml
