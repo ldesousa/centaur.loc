63d33d494e287c856f0c52b54bc42ee6261e4835
Luís de Sousa <luis.de.sousa@protonmail.ch>
2017-02-10 14:53:38 +0100

    Updated SQL syntax for Psotgres 9.

--- db/0602_grants.sql
+++ db/0602_grants.sql
@@ -1,15 +1,17 @@
-﻿GRANT USAGE ON SCHEMA centaur to centaur;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA alcantara TO desouslu;
 
-GRANT SELECT ON ALL TABLES IN SCHEMA centaur TO centaur;
+﻿GRANT USAGE ON SCHEMA alcantara to centaur;
 
-GRANT SELECT ON centaur.v_candidate TO centaur;
-GRANT SELECT ON centaur.v_conduit TO centaur;
-GRANT SELECT ON centaur.v_flooded TO centaur;
-GRANT SELECT ON centaur.v_junction TO centaur;
-GRANT SELECT ON centaur.v_outfall TO centaur;
-GRANT SELECT ON centaur.v_pump TO centaur;
-GRANT SELECT ON centaur.v_storage TO centaur;
-GRANT SELECT ON centaur.v_weir TO centaur;
+GRANT SELECT ON ALL TABLES IN SCHEMA alcantara TO centaur;
+
+GRANT SELECT ON alcantara.v_candidate TO centaur;
+GRANT SELECT ON alcantara.v_conduit TO centaur;
+GRANT SELECT ON alcantara.v_flooded TO centaur;
+GRANT SELECT ON alcantara.v_junction TO centaur;
+GRANT SELECT ON alcantara.v_outfall TO centaur;
+GRANT SELECT ON alcantara.v_pump TO centaur;
+GRANT SELECT ON alcantara.v_storage TO centaur;
+GRANT SELECT ON alcantara.v_weir TO centaur;
  
 
 
