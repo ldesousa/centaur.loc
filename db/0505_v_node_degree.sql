fa4dd9e30297b437b5ee6256b0aba9cb7141bd8b
Luís de Sousa <luis.de.sousa@protonmail.ch>
2017-03-08 17:28:45 +0100

    Adding node degree computation.

---
+++ db/0505_v_node_degree.sql
@@ -0,0 +1,22 @@
+﻿SET search_path TO alcantara, public;
+
+CREATE OR REPLACE VIEW v_node_degree AS
+SELECT f.id_node_from AS id_node,
+       COALESCE(f.count, 0) + COALESCE(t.count, 0) AS degree
+  FROM  (SELECT id_node_from, COUNT(*) 
+	   FROM alcantara.link 
+	  GROUP BY id_node_from) f
+  FULL OUTER JOIN	
+        (SELECT id_node_to, COUNT(*) 
+	   FROM alcantara.link 
+	  GROUP BY id_node_to) t
+    ON t.id_node_to = f.id_node_from;  
+
+SELECT AVG(degree),
+       STDDEV(degree),       
+       MAX(degree),
+       MIN(degree) 
+  FROM v_node_degree;
+
+
+
