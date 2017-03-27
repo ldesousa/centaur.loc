8d123142f086dd2fb3484f9d66dcd0775cba81f7
Luís de Sousa <luis.de.sousa@protonmail.ch>
2017-03-27 17:04:39 +0200

    Made node degree view generic.

--- db/0505_v_node_degree.sql
+++ db/0505_v_node_degree.sql
@@ -1,14 +1,14 @@
-﻿SET search_path TO alcantara, public;
+﻿SET search_path TO coimbra, public;
 
 CREATE OR REPLACE VIEW v_node_degree AS
 SELECT f.id_node_from AS id_node,
        COALESCE(f.count, 0) + COALESCE(t.count, 0) AS degree
   FROM  (SELECT id_node_from, COUNT(*) 
-	   FROM alcantara.link 
+	   FROM link 
 	  GROUP BY id_node_from) f
   FULL OUTER JOIN	
         (SELECT id_node_to, COUNT(*) 
-	   FROM alcantara.link 
+	   FROM link 
 	  GROUP BY id_node_to) t
     ON t.id_node_to = f.id_node_from;  
 
