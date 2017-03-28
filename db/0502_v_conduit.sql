eb27b0aeb066b3a759e2525fdca9ef444bc98a78
Luís de Sousa <luis.de.sousa@protonmail.ch>
2017-03-28 15:56:38 +0200

    New energy slope calculation with q_max from upstream and downstream conduits.

--- db/0502_v_conduit.sql
+++ db/0502_v_conduit.sql
@@ -19,6 +19,7 @@ SELECT c.id_link,
    AND nf.id = l.id_node_from
    AND nt.id = l.id_node_to;
 
+-- SELECT COUNT(*) FROM v_conduit_slope;
 
 CREATE OR REPLACE VIEW v_conduit_q_max AS
 SELECT c.id_link,
@@ -29,12 +30,13 @@ SELECT c.id_link,
        area * c.length AS volume,
        1.0/0.015 * area * power(area / perimeter, 2.0/3.0) * power(abs(s.slope), 1.0/2.0) AS q_max
   FROM conduit c,
-       xsection x,
        v_conduit_slope s,
+       xsection x,
        LATERAL (SELECT pi() * ((x.geom1/2) ^ 2),
                        2 * pi() * (x.geom1/2) )
 		    AS s1(area, perimeter)
- WHERE x.id_link = c.id_link
+ WHERE c.id_link = s.id_link
+   AND c.id_link = x.id_link
    AND x.shape LIKE 'CIRC%'
    AND x.geom1 IS NOT NULL
  UNION
@@ -46,12 +48,13 @@ SELECT c.id_link,
        area * c.length AS volume,
        1.0/0.015 * power(area / perimeter, 2.0/3.0) * power(abs(s.slope), 1.0/2.0) AS q_max
   FROM conduit c,
-       xsection x,
        v_conduit_slope s,
+       xsection x,
        LATERAL (SELECT pi() * ((x.geom1/2) * (x.geom2/2)),
                        2 * pi() * sqrt((x.geom1^2 + x.geom2^2)/2) )
 		    AS s1(area, perimeter)
- WHERE x.id_link = c.id_link
+ WHERE c.id_link = s.id_link
+   AND c.id_link = x.id_link
    AND x.shape LIKE 'EGG%'
    AND x.geom1 IS NOT NULL
    AND x.geom2 IS NOT NULL
@@ -64,16 +67,18 @@ SELECT c.id_link,
        area * c.length AS volume,
        1.0/0.015 * power(area / perimeter, 2.0/3.0) * power(abs(s.slope), 1.0/2.0) AS q_max
   FROM conduit c,
-       xsection x,
        v_conduit_slope s,
+       xsection x,
        LATERAL (SELECT x.geom1 * x.geom2,
                        2 * x.geom1 + 2 * x.geom2 )
 		    AS s1(area, perimeter)
- WHERE x.id_link = c.id_link
+ WHERE c.id_link = s.id_link
+   AND c.id_link = x.id_link
    AND x.shape LIKE 'RECT%'
    AND x.geom1 IS NOT NULL
    AND x.geom2 IS NOT NULL;
    
+-- SELECT COUNT(*) FROM v_conduit_q_max;
    
 CREATE OR REPLACE VIEW v_conduit AS
 SELECT l.id,
@@ -90,25 +95,31 @@ SELECT l.id,
        s.slope,
        q.area,
        q.perimeter,
-       (((q.q_max + lf.q_max + lt.q_max) / 3) * 0.015 / ((q.area / q.perimeter) ^ (2/3))) ^ 2 AS energy_slope
+       q.area * c.length::double precision AS volume,
+       q.q_max,
+       (((q.q_max + COALESCE(lf.q_max, 0) + COALESCE(lt.q_max, 0)) / 
+         (1 + COALESCE(lt.q_max / lt.q_max, 0) + COALESCE(lt.q_max / lt.q_max, 0))) * 
+        0.015 / ((q.area / q.perimeter) ^ (2/3::double precision))) ^ 2 AS energy_slope
   FROM conduit c,
-       link l,
        v_conduit_slope s,
        v_conduit_q_max q,
-       (SELECT q.id_node_to, 
-               MAX(q.q_max) AS q_max
-          FROM v_conduit_q_max q
-         GROUP BY q.id_node_to) lf,
-       (SELECT q.id_node_from, 
-               MAX(q.q_max) AS q_max
-          FROM v_conduit_q_max q
-         GROUP BY q.id_node_from) lt
+       link l
+  LEFT JOIN (SELECT q.id_node_to, 
+		    MAX(q.q_max) AS q_max
+	       FROM v_conduit_q_max q
+	      GROUP BY q.id_node_to) lf
+    ON l.id_node_from = lf.id_node_to
+  LEFT JOIN (SELECT q.id_node_from, 
+		    MAX(q.q_max) AS q_max
+	       FROM v_conduit_q_max q
+	      GROUP BY q.id_node_from) lt
+    ON l.id_node_to = lt.id_node_from
  WHERE c.id_link = l.id
    AND c.id_link = s.id_link
-   AND c.id_link = q.id_link
-   AND lf.id_node_to = l.id_node_from
-   AND lt.id_node_from = l.id_node_to;
-   
+   AND c.id_link = q.id_link;
+
+-- SELECT COUNT(*) FROM v_conduit_temp;
+ 
  
 -- Check volumes
 SELECT c.id, sum(l.volume)
@@ -118,3 +129,6 @@ SELECT c.id, sum(l.volume)
  WHERE l.id = f.id_link
    AND c.id = f.id_node
  GROUP BY(c.id);
+
+
+SELECT * FROM v_conduit_temp WHERE name LIKE '63.1';
