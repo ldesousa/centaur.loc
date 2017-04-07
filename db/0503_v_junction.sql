72049e87b221856efc88c16a47f7d7c2a5375dd3
Luís de Sousa <luis.de.sousa@protonmail.ch>
2017-04-07 14:21:55 +0200

    Updated v_flooded with new fields.

--- db/0503_v_junction.sql
+++ db/0503_v_junction.sql
@@ -25,14 +25,21 @@ SELECT n.id AS id_node,
 
 -- SELECT COUNT(*) FROM v_junction;
 
+-- DROP VIEW v_flooded; 
 CREATE OR REPLACE VIEW v_flooded AS
 SELECT f.id_flooded,
        f.id_node as id_node_candidate,
        l.id,
-       l.name,
+       l.name as link_name,
        l.id_node_from,
        l.id_node_to,
+       f.q_prac,
+       f.energy_line_offset,
        l.geom
   FROM flooded f,
        link l	
  WHERE f.id_link = l.id;
+ 
+-- SELECT * FROM v_flooded WHERE id_node_candidate = -545475706;
+ 
+ 
